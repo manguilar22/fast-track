3d7ff7510df3fadac0d001e53d570839c8bb093e
NULL <manguilar22@gmail.com>
2020-05-23 13:18:02 -0600

    start

---
+++ src/test/java/guru/aguilar/fasttrack/FastTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.aguilar.fasttrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
